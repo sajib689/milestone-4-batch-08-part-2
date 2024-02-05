ad1ec4bb1696dfb0471b1d6a1de14047012524aa
sajib689 <jibonraja11@gmail.com>
2024-02-05 16:46:51 +0600

    max

---
+++ index.js
@@ -0,0 +1,17 @@
+const disha = 56
+const salamn = 95 
+
+if(disha > salamn) {
+    console.log('Disha get the top of the list')
+} else {
+    console.log('salamn eat the stobery')
+}
+
+function whoGetTopMark (a, b) {
+    if(a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+console.log(whoGetTopMark(100, 102))
